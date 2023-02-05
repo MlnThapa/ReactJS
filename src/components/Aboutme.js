92bc95514640ca32090598f67da8f516353d8120
Milan Thapa <thapamilan584@gmail.com>
2023-02-06 02:03:00 +0530

    Change in Paragraph in my details tag

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -46,10 +46,12 @@ function Aboutme() {
                     <div className="image-div fadeIn2"></div>
                     <div className="my-details fadeIn2">
                         <div style={{display:'flex',}}><p>Hi! I'm </p><p style={{color:'#b40d1a',marginLeft:`${4}px`,fontWeight:600}}>Milan Thapa</p></div>
-                        <p>I'm a computer science engineer who is passionate about </p>
-                        <p>designing, photography and photo editting.</p>
-                        <p>Being a beginner at web designing and developing I'm just trying to</p>
-                        <p>survive with noob experience and pro confidence.</p>
+                        <p>I am a recent graduate with a degree in Computer Science and a passion for web development.</p>
+                        <p>I have hands-on experience in HTML, CSS, JavaScript, and various web development frameworks.</p>
+                        <p>such as React and Vue.I am a quick learner and always eager to dive into new technologies.</p>
+                        <p>I have a strong understanding of software development principles and an ability to work in a</p>
+                        <p>team environment. My goal is to continuously improve my skills and deliver high-quality, </p>
+                        <p>user-friendly web applications.</p>
                     </div>
                 </div>   
             </div>
